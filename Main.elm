c392bc461aa8e25eacba370d9c0a1a24c32dc5dc
Emilien Taque <eta@zengularity.com>
2016-12-01 23:44:46 +0100

    with watcher and list of hexagons

--- Main.elm
+++ Main.elm
@@ -1,22 +1,51 @@
 module Main exposing (..)
 
-import Math.Vector2 exposing (..)
+import Math.Vector2 as Vec2 exposing (..)
 import WebGL exposing (..)
 import Html exposing (Html)
 import Html.Attributes exposing (width, height)
 import AnimationFrame
 import Time exposing (Time)
 import Hexagons
+import Hexagons.Grid as Grid exposing (Grid)
+import Dict
+import Window
+import Task
 
 
--- Create a mesh with two triangles
+type Msg
+    = WindowSize Window.Size
+    | Elapsed Time
 
 
-type alias Vertex =
-    { position : Vec2 }
+type alias Model =
+    { time : Time
+    , size : Window.Size
+    , grid : Grid WindCell
+    }
 
 
-mesh : Drawable Vertex
+initialModel : Model
+initialModel =
+    Model 0 { width = 100, height = 100 } grid
+
+
+type alias WindCell =
+    { speed : Float }
+
+
+grid : Grid.Grid WindCell
+grid =
+    Dict.empty
+        |> Grid.set (WindCell 2) ( -1, 0 )
+        |> Grid.set (WindCell 4) ( 0, 0 )
+        |> Grid.set (WindCell 6) ( 1, 0 )
+        |> Grid.set (WindCell 8) ( 2, 0 )
+        |> Grid.set (WindCell 1) ( 0, 1 )
+        |> Grid.set (WindCell 3) ( 1, 1 )
+
+
+mesh : Drawable Attribute
 mesh =
     let
         ( w, h ) =
@@ -32,59 +61,105 @@ mesh =
             h / 4
     in
         Triangle
-            [ ( Vertex (vec2 -w2 -h4), Vertex (vec2 0 -h2), Vertex (vec2 0 0) )
-            , ( Vertex (vec2 0 -h2), Vertex (vec2 w2 -h4), Vertex (vec2 0 0) )
-            , ( Vertex (vec2 w2 -h4), Vertex (vec2 w2 h4), Vertex (vec2 0 0) )
-            , ( Vertex (vec2 w2 h4), Vertex (vec2 0 h2), Vertex (vec2 0 0) )
-            , ( Vertex (vec2 0 h2), Vertex (vec2 -w2 h4), Vertex (vec2 0 0) )
-            , ( Vertex (vec2 -w2 h4), Vertex (vec2 -w2 -h4), Vertex (vec2 0 0) )
+            [ ( Attribute (vec2 -w2 -h4), Attribute (vec2 0 -h2), Attribute (vec2 0 0) )
+            , ( Attribute (vec2 0 -h2), Attribute (vec2 w2 -h4), Attribute (vec2 0 0) )
+            , ( Attribute (vec2 w2 -h4), Attribute (vec2 w2 h4), Attribute (vec2 0 0) )
+            , ( Attribute (vec2 w2 h4), Attribute (vec2 0 h2), Attribute (vec2 0 0) )
+            , ( Attribute (vec2 0 h2), Attribute (vec2 -w2 h4), Attribute (vec2 0 0) )
+            , ( Attribute (vec2 -w2 h4), Attribute (vec2 -w2 -h4), Attribute (vec2 0 0) )
             ]
 
 
-main : Program Never Time Time
+main : Program Never Model Msg
 main =
     Html.program
-        { init = ( 0, Cmd.none )
+        { init = ( initialModel, Task.perform WindowSize Window.size )
         , view = view
         , subscriptions =
-            \model -> Sub.none
+            \model -> Window.resizes WindowSize
             -- (\model -> AnimationFrame.diffs Basics.identity)
-        , update = (\elapsed currentTime -> ( elapsed + currentTime, Cmd.none ))
+        , update = update
         }
 
 
-view : Float -> Html msg
-view t =
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        WindowSize size ->
+            { model | size = size } ! []
+
+        Elapsed delta ->
+            { model | time = model.time + delta } ! []
+
+
+hexRadius : Float
+hexRadius =
+    50
+
+
+view : Model -> Html msg
+view model =
     WebGL.toHtml
-        [ width 400, height 400 ]
-        [ render vertexShader fragmentShader mesh {} ]
+        [ width model.size.width, height model.size.height ]
+        (List.map (renderCell model.size) (Grid.list model.grid))
 
 
+renderCell : Window.Size -> Grid.Tile WindCell -> Renderable
+renderCell { width, height } tile =
+    let
+        uniform =
+            { speed = tile.content.speed
+            , offset = Vec2.fromTuple (Hexagons.axialToPoint hexRadius tile.coords)
+            , windowSize = vec2 (toFloat width) (toFloat height)
+            , hexSize = Vec2.fromTuple (Hexagons.dims hexRadius)
+            }
+    in
+        render vertexShader fragmentShader mesh (Debug.log "uniform" uniform)
 
--- Shaders
+
+type alias Attribute =
+    { position : Vec2 }
 
 
-vertexShader : Shader { attr | position : Vec2 } {} {}
+type alias Uniform u =
+    { u
+        | speed : Float
+        , offset : Vec2
+        , windowSize : Vec2
+        , hexSize : Vec2
+    }
+
+
+type alias Varying =
+    { vSpeed : Float }
+
+
+vertexShader : Shader Attribute (Uniform u) Varying
 vertexShader =
     [glsl|
+  attribute vec2 position;
+  uniform float speed;
+  uniform vec2 offset;
+  uniform vec2 hexSize;
+  uniform vec2 windowSize;
+  varying float vSpeed;
 
-attribute vec2 position;
-
-void main () {
-    gl_Position = vec4(position, 0, 1);
-}
-
+  void main () {
+      vec2 clipSpace = position * vec2(hexSize.y, hexSize.y) / windowSize;
+      vec2 offsetClipSpace = offset / windowSize * 2.0;
+      gl_Position = vec4(vec2(clipSpace.x + offsetClipSpace.x, clipSpace.y - offsetClipSpace.y), 0, 1);
+      vSpeed = speed;
+  }
 |]
 
 
-fragmentShader : Shader {} u {}
+fragmentShader : Shader {} u Varying
 fragmentShader =
     [glsl|
+  precision mediump float;
+  varying float vSpeed;
 
-precision mediump float;
-
-void main () {
-    gl_FragColor = vec4(0, 1, 0, 1);
-}
-
+  void main () {
+      gl_FragColor = vec4(0, 1.0 - vSpeed * 0.1, 0, 1);
+  }
 |]
